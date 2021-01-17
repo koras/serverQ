c64eb2403e6db8790a456c26e5c2b653c22d47b9
koras <kos6476@yandex.ru>
2021-01-18 01:01:08 +0300

    create rep

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests; 
+use App\Http\Contracts\UserServiceInterface;
+
+use Illuminate\Http\Request;
+
+class UserController extends QuestionController
+{ 
+
+    private $userService;
+
+    public function __construct(UserServiceInterface $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function  check(Request $request)
+    {
+        
+        return $this->userService;
+    }
+}
---
+++ app/Http/Services/UserService.php
@@ -0,0 +1,13 @@
+<?php
+
+ 
+namespace App\Http\Services;
+
+use App\Http\Contracts\UserServiceInterface;
+
+
+class UserService   implements UserServiceInterface
+{
+    
+    
+}
